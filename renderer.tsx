6c89f9ef8e0cdf432df48eca06cfaa96ca1b3025
Daniel Pacak <pacak.daniel@gmail.com>
2020-11-09 15:57:17 +0100

    feat: Register Starboard cluster feature so we can install / uninstall the extension
    Signed-off-by: Daniel Pacak <pacak.daniel@gmail.com>

--- renderer.tsx
+++ renderer.tsx
@@ -1,8 +1,26 @@
 import {LensRendererExtension} from "@k8slens/extensions";
 import {ExampleIcon, StarboardPage} from "./page"
 import React from "react"
+import {StarboardFeature} from "./starboard-feature"
 
-export default class ExampleExtension extends LensRendererExtension {
+export default class StarboardExtension extends LensRendererExtension {
+    clusterFeatures = [
+        {
+            title: "Starboard",
+            components: {
+                Description: () => {
+                    return (
+                        <span>
+                            Enable Kubernetes-native security (Starboard toolkit) for your cluster.
+                            Install only if Starboard has not been initialized yet.
+                            The install command will create Kubernetes resources used by Starboard.
+                        </span>
+                    )
+                }
+            },
+            feature: new StarboardFeature()
+        }
+    ]
     clusterPages = [
         {
             path: "/extension-starboard",
