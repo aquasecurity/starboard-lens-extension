b832af1588bf0fe49a77ddb9abdb614791bd4a5a
Daniel Pacak <pacak.daniel@gmail.com>
2020-11-11 20:43:42 +0100

    feat: Define ConfigAuditReport
    Signed-off-by: Daniel Pacak <pacak.daniel@gmail.com>

---
+++ src/components/configaudit-reports-list.tsx
@@ -0,0 +1,50 @@
+import {Component, LensRendererExtension} from "@k8slens/extensions";
+import React from "react";
+import {configAuditReportsStore} from "../configaudit-report-store";
+import {ConfigAuditReport} from "../configaudit-report";
+
+enum sortBy {
+    name = "name",
+    namespace = "namespace",
+    danger = "danger",
+    warning = "warning",
+}
+
+export class ConfigAuditReportPage extends React.Component<{ extension: LensRendererExtension }> {
+
+    async componentDidMount() {
+        await configAuditReportsStore.loadAll()
+    }
+
+    render() {
+        return (
+            <Component.KubeObjectListLayout
+                className="ConfigAuditReports" store={configAuditReportsStore}
+                sortingCallbacks={{
+                    [sortBy.name]: (report: ConfigAuditReport) => report.getName(),
+                    [sortBy.namespace]: (report: ConfigAuditReport) => report.metadata.namespace,
+                    [sortBy.danger]: (report: ConfigAuditReport) => report.report.summary.dangerCount,
+                    [sortBy.warning]: (report: ConfigAuditReport) => report.report.summary.warningCount,
+                }}
+                searchFilters={[
+                    (report: ConfigAuditReport) => report.getSearchFields()
+                ]}
+                renderHeaderTitle="ConfigAuditReports"
+                renderTableHeader={[
+                    {title: "Name", className: "name", sortBy: sortBy.name},
+                    {title: "Namespace", className: "namespace", sortBy: sortBy.namespace},
+                    {title: "Scanner", className: "scanner"},
+                    {title: "Danger", className: "danger", sortBy: sortBy.danger},
+                    {title: "Warning", className: "warning", sortBy: sortBy.warning},
+                ]}
+                renderTableContents={(report: ConfigAuditReport) => [
+                    report.getName(),
+                    report.metadata.namespace,
+                    report.report.scanner.name + " " + report.report.scanner.version,
+                    report.report.summary.dangerCount,
+                    report.report.summary.warningCount,
+                ]}
+            />
+        )
+    }
+}
